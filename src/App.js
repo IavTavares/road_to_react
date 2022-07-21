ebe8d3f49c8dc711ed8c166006b6ea2eba279c0f
I T <ivo.a.v.tavares@gmail.com>
2022-07-21 15:56:06 +0100

    Used props to pass down information to components in the component tree.

--- src/App.js
+++ src/App.js
@@ -29,15 +29,32 @@ const authorStyle = {
 
 
 function App() {
-
+  const stories = [
+    {
+      title: 'React_story',
+      url: 'https://reactjs.org',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux_story',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
   return (
     <div>
       <h1> My Hacker Stories </h1>
       <Search />
       <hr />
-      <List />
+      <List list={stories}/>
       <hr />
-      <List />
+      <List list={list}/>
     </div>
   )
 };
@@ -53,24 +70,36 @@ function Search(){
     </div>
   )
 };
-function List(){
+function List(props){
   return (
     <ul>
-      {list.map(function (item){
-        return(
-          <li key = {item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}:</a>
-          </span>
-          <span style={authorStyle}>  {item.author},</span>
-          <span> {item.num_comments},</span>
-          <span> {item.points}.</span>
-          </li>
+      {
+        props.list.map(
+          function (item){
+            return(
+              <Item key = {item.objectID} item = {item}/>
+            )
+          }
         )
-      })} 
-    </ul>  
+      }
+    </ul> 
   );
 }
+function Item(props){
+  const keys = Object.keys(props);
+  console.log("properties of Item Element:" + keys)
+  return(
+    <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}:</a>
+    </span>
+    <span style={authorStyle}>  {props.item.author},</span>
+    <span> {props.item.num_comments},</span>
+    <span> {props.item.points}.</span>
+    </li>
+  )
+   
+};
 
 
 
